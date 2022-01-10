21c0a06ef96b7c4ac1231ce2fd29c109526ad897
yitong86 <YOUR_EMAIL_ADDRESS>
2022-01-10 15:47:50 -0500

    algorithms

--- src/com/sophia/Array.java
+++ src/com/sophia/Array.java
@@ -18,63 +18,31 @@ public class Array {
 
 
 
-        /*
-         * Complete the 'hourglassSum' function below.
-         *
-         * The function is expected to return an INTEGER.
-         * The function accepts 2D_INTEGER_ARRAY arr as parameter.
-         */
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
 
-        public static int hourglassSum(int[][] arr) {
-            // Write your code here
-            int rows = arr.length;
-            int columns = arr[0].length;
+    public static int hourglassSum(List<List<Integer>> arr) {
+        // Write your code here
 
-            int max_hourglass_sum = Integer.MIN_VALUE;
+        int maxHourglass = arr.get(0).get(0) + arr.get(0).get(1) + arr.get(0).get(2) + arr.get(1).get(1) + arr.get(2).get(0) + arr.get(2).get(1) + arr.get(2).get(2);
 
-            for (int i = 0; i < rows-2; i++) {
-                for (int j = 0; j < columns-2;j++) {
-                    int current_hourglass_sum =
-                            arr[i][j]+ arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j+1]+arr[i+2][j+2];
-                    if (max_hourglass_sum < current_hourglass_sum){
-                        max_hourglass_sum = current_hourglass_sum;
-                    }
-                    //  max_hourglass_sum = Math.max(max_hourglass_sum,current_hourglass_sum);
-
+        for (int i = 0; i < arr.size() - 2; i++) {
+            for (int j = 0; j < arr.size() - 2; j++) {
+                int current_hourglass_sum = arr.get(i).get(j) + arr.get(i).get(j+1) +arr.get(i).get(j+2) + arr.get(i+1).get(j+1) + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j+2);
+                        ;
+                if (maxHourglass < current_hourglass_sum) {
+                    maxHourglass = current_hourglass_sum;
                 }
-            }
-            return max_hourglass_sum;
-        }
+                //  max_hourglass_sum = Math.max(max_hourglass_sum,current_hourglass_sum);
 
-
-
-        public class Solution {
-            public static void main(String[] args) throws IOException {
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-                BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-                List<List<Integer>> arr = new ArrayList<>();
-
-                IntStream.range(0, 6).forEach(i -> {
-                    try {
-                        arr.add(
-                                Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                                        .map(Integer::parseInt)
-                                        .collect(toList())
-                        );
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
-                    }
-                });
-
-                int result = Array.hourglassSum(arr);
-
-                bufferedWriter.write(String.valueOf(result));
-                bufferedWriter.newLine();
-
-                bufferedReader.close();
-                bufferedWriter.close();
             }
         }
+        return maxHourglass;
+    }
+
 
 }
--- src/com/sophia/alternatingCharacters.java
+++ src/com/sophia/alternatingCharacters.java
@@ -35,27 +35,3 @@ public class alternatingCharacters {
 
     }
 
-    public class Solution {
-        public static void main(String[] args) throws IOException {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-            int q = Integer.parseInt(bufferedReader.readLine().trim());
-
-            IntStream.range(0, q).forEach(qItr -> {
-                try {
-                    String s = bufferedReader.readLine();
-
-                    int result = alternatingCharacters.alternatingCharacters(s);
-
-                    bufferedWriter.write(String.valueOf(result));
-                    bufferedWriter.newLine();
-                } catch (IOException ex) {
-                    throw new RuntimeException(ex);
-                }
-            });
-
-            bufferedReader.close();
-            bufferedWriter.close();
-        }
-    }
