e4167a8c0ca6e6eea4c4d2bca48eb612525f8c4d
yitong86 <sophiajiang86@hotmail.com>
2022-02-18 16:24:02 -0500

    ReturnSum

---
+++ src/com/sophia/ReturnSum.java
@@ -0,0 +1,14 @@
+package com.sophia;
+
+public class ReturnSum {
+    public static void main(String[] args) {
+        System.out.println(sum(3 ,2));
+    }
+
+        public static int sum(int a, int b) {
+            int sum = 0;
+            sum = a + b;
+            return sum;
+        }
+    }
+
