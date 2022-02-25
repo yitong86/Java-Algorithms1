839c80d6f2438b65c9c54d8a214f7702545f5f4d
yitong86 <sophiajiang86@hotmail.com>
2022-02-25 14:26:38 -0500

    PowerCalculator

---
+++ src/com/sophia/PowerCalculator.java
@@ -0,0 +1,9 @@
+package com.sophia;
+
+public class PowerCalculator {
+        public static int power(int voltage, int current) {
+            int sum = voltage * current;
+            return sum;
+        }
+    }
+
