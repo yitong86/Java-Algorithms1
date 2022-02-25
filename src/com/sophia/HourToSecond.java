7019731f63530f84d0aff583ca465ead8f0a443f
yitong86 <sophiajiang86@hotmail.com>
2022-02-25 13:59:11 -0500

    HourToSecond

---
+++ src/com/sophia/HourToSecond.java
@@ -0,0 +1,10 @@
+package com.sophia;
+
+public class HourToSecond {
+        public static int howManySeconds(int hours) {
+            int seconds = hours * 60 *60;
+            return seconds;
+
+        }
+    }
+
