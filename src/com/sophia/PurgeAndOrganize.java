84794689cb8758e0ae8f0f0570afa811ad386e01
yitong86 <sophiajiang86@hotmail.com>
2022-02-27 17:18:09 -0500

    PurgeAndOrganize

--- src/com/sophia/PurgeAndOrganize.java
+++ src/com/sophia/PurgeAndOrganize.java
@@ -6,31 +6,37 @@ public class PurgeAndOrganize {
 
     public static void main(String[] args) {
         int[] nums = {1, 4, 4, 4, 4, 4, 3, 2, 1, 2};
-        for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]);
+        System.out.println( Arrays.toString(uniqueSort(nums)));
+
+    }
+
+
+        public static int[] uniqueSort(int[] nums) {
+            Arrays.sort(nums);
+            int n = nums.length;
+            if (n==0 || n==1){
+                return nums;
+            }
+            int[] temp = new int[n];
+            int j = 0;
+            for (int i=0; i<n-1; i++){
+                if (nums[i] != nums[i+1]){
+                    temp[j] = nums[i];
+                    j++;
+                }
+            }
+            temp[j] = nums[n-1];
+            j++;
+            //j =4
+            int[] newArr = new int[j];
+            // Changing original array
+            for (int i=0; i < j; i++){
+                newArr[i] = temp[i];
+            }
+            return newArr;
         }
     }
 
-    public static int[] uniqueSort(int[] nums, int n) {
-        Arrays.sort(nums);
-        if (n == 0 || n == 1) {
-            System.out.println(n);
-        }
-        int[] temp = new int[n];
-        int j = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] != nums[i + 1]) {
-                temp[j++] = nums[i];
-            }
-        }
-        temp[j++] = nums[n - 1];
-        // Changing original array
-        for (int i = 0; i < j; i++) {
-            nums[i] = temp[i];
-        }
-        return nums;
-    }
-}
 //    uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
 //
 //        uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
