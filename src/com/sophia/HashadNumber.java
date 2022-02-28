47a26c8af86210cde0aea1f4d427796cfce02625
yitong86 <sophiajiang86@hotmail.com>
2022-02-28 13:26:29 -0500

    HashadNumber

--- src/com/sophia/HashadNumber.java
+++ src/com/sophia/HashadNumber.java
@@ -2,14 +2,18 @@ package com.sophia;
 
 public class HashadNumber {
   //  public class HarshadNumber {
+  public static void main(String[] args) {
+      System.out.println(isHarshad(75));
+  }
         public static boolean isHarshad(int n) {
-            int num = 0;
-            int num1 = n;
-            while(n > 0){
-                num = num + n % 10;
-                n = n / 10;
+            int sum = 0;
+            int number = n;//->123//->12//->1//->0 end of wile loop
+            while(n > 0){//1234 > 0  //123//12//1
+                int lastDigital = n % 10;//4  //3 //2 //1
+                sum = sum +lastDigital;//0+4=4 // 4+3=7 //4+3+2=9 //4+3+2+1=10
+                n = n / 10;//1234/10=123.4 update number//12.3 // 1.2 //0
             }
-            return num1 % num == 0;
+            return number % sum == 0 ? true:false;//why we cannot use n instead of number?
         }
     }
 
